da1c89caebea0ead84d41db5d536ab0b7236132f
Andrew Chaa <andrew.yh.chaa@gmail.com>
2024-04-27 00:25:50 +0100

    Add total_macd_trade_signal function to common_trade_signals.py

--- ddd-southwest/common_trade_signals.py
+++ ddd-southwest/common_trade_signals.py
@@ -1,3 +1,12 @@
+def total_macd_trade_signal(row):
+    if (row.MACD_trade_signal == 1):
+        return 1
+    elif (row.MACD_trade_signal == -1):
+        return -1
+    else:
+        return 0
+
+
 def total_macd_adx_trade_signal(row):
     if (row.ADX_trade_signal == 0):
         return 0
